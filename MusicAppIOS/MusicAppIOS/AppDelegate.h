6197ce52f5b6561f5a0d52b2432b6e412aa2f5bf
Roman <Roman@Romans-MacBook-Pro.local>
2016-06-19 17:45:41 +0700

    Create project

---
+++ MusicAppIOS/MusicAppIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicAppIOS
+//
+//  Created by Roman on 19/06/16.
+//  Copyright © 2016 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicAppIOS/MusicAppIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicAppIOS
+//
+//  Created by Roman on 19/06/16.
+//  Copyright © 2016 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
